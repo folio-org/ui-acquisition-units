422798e1e94e6ce528345808b6e838398e573c1a
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2020-01-31 16:55:54 +0300

    fix tests (#50)

--- test/bigtest/interactors/AcquisitionUnitDetails.js
+++ test/bigtest/interactors/AcquisitionUnitDetails.js
@@ -7,7 +7,7 @@ import {
 export default interactor(class AcquisitionUnitDetailsInteractor {
   static defaultScope = '[data-test-ac-unit-details]';
 
-  paneHeaderCenterButton = new Interactor('[class*=paneHeaderCenterButton---]');
+  paneHeaderCenterButton = new Interactor('[data-test-pane-header-actions-button]');
   editUnitAction = new Interactor('[data-test-ac-unit-details-edit-action]');
 
   isLoaded = isPresent('[data-test-ac-unit-details-accordions]');
