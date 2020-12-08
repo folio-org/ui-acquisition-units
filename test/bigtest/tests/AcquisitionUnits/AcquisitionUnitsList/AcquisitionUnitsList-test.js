9b8a91dc02a50b0023b387fb1dee265af49c39f7
Andrei Shumski <Andrei_Shumski2@epam.com>
2020-12-08 13:43:39 +0300

    test

--- test/bigtest/tests/AcquisitionUnits/AcquisitionUnitsList/AcquisitionUnitsList-test.js
+++ test/bigtest/tests/AcquisitionUnits/AcquisitionUnitsList/AcquisitionUnitsList-test.js
@@ -4,6 +4,7 @@ import { expect } from 'chai';
 import setupApplication from '../../../helpers/setup-application';
 import AcquisitionUnitsListInteractor from '../../../interactors/AcquisitionUnitsList';
 import AcquisitionUnitEditorInteractor from '../../../interactors/AcquisitionUnitEditor';
+import AcquisitionUnitDetailsInteractor from '../../../interactors/AcquisitionUnitDetails';
 
 const UNITS_COUNT = 15;
 
@@ -41,6 +42,19 @@ describe('Acquisition units list', () => {
 
     it('should open unit editor', () => {
       expect(acquisitionUnitEditor.isVisible).to.be.true;
+    });
+  });
+
+  describe('view acquisition unit deatils', () => {
+    const acquisitionUnitDetails = new AcquisitionUnitDetailsInteractor();
+
+    beforeEach(async function () {
+      await acquisitionUnitsList.units(0).click();
+      await acquisitionUnitDetails.whenLoaded();
+    });
+
+    it('should open unit details', () => {
+      expect(acquisitionUnitDetails.isVisible).to.be.true;
     });
   });
 });
