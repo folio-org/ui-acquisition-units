7ef973e44811dc886ba259631478ee4e1ae478c8
Yury Saukou <yury_saukou@epam.com>
2025-06-16 13:19:06 +0400

    Tech debt: increase test coverage (#210)

--- src/settings/AcquisitionUnits/AcquisitionUnitMemberships/AcquisitionUnitMembershipsContainer.test.js
+++ src/settings/AcquisitionUnits/AcquisitionUnitMemberships/AcquisitionUnitMembershipsContainer.test.js
@@ -1,29 +1,82 @@
 import { MemoryRouter } from 'react-router-dom';
 
-import { render } from '@folio/jest-config-stripes/testing-library/react';
+import {
+  render,
+  screen,
+} from '@folio/jest-config-stripes/testing-library/react';
+import userEvent from '@folio/jest-config-stripes/testing-library/user-event';
+import { Pluggable } from '@folio/stripes/core';
 
 import AcquisitionUnitMembershipsContainer from './AcquisitionUnitMembershipsContainer';
 
-jest.mock('./AcquisitionUnitMemberships', () => {
-  return () => <span>AcquisitionUnitMemberships</span>;
-});
+const user = { id: '1', personal: { lastName: 'Doe', firstName: 'John' } };
 
-const renderAcquisitionUnitMembershipsContainer = ({
-  resources = {},
-  mutator = {},
-} = {}) => (render(
+const defaultProps = {
+  mutator: {
+    acquisitionsUnitMemberships: {
+      POST: jest.fn(() => Promise.resolve()),
+      DELETE: jest.fn(() => Promise.resolve()),
+    },
+    users: {
+      GET: jest.fn(() => Promise.resolve([user])),
+    },
+  },
+  resources: {
+    acquisitionsUnitMemberships: {
+      records: [{ userId: '1' }],
+    },
+    patronGroups: {
+      records: [{ id: '1', desc: 'qwerty' }],
+    },
+  },
+};
+
+const selectUsersLabel = 'Select users';
+
+const renderAcquisitionUnitMembershipsContainer = (props = {}) => (render(
   <MemoryRouter>
     <AcquisitionUnitMembershipsContainer
-      resources={resources}
-      mutator={mutator}
+      {...defaultProps}
+      {...props}
     />
   </MemoryRouter>,
 ));
 
 describe('AcquisitionUnitMembershipsContainer', () => {
-  it('should render AcquisitionUnitMemberships', () => {
-    const { getByText } = renderAcquisitionUnitMembershipsContainer();
+  beforeEach(() => {
+    Pluggable.mockImplementation(({ selectUsers }) => (
+      <button
+        type="button"
+        onClick={() => {
+          selectUsers([{ id: '1' }]);
+        }}
+      >
+        {selectUsersLabel}
+      </button>
+    ));
+  });
 
-    expect(getByText('AcquisitionUnitMemberships')).toBeDefined();
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render users plugin trigger', () => {
+    renderAcquisitionUnitMembershipsContainer();
+
+    expect(screen.getByText(selectUsersLabel)).toBeInTheDocument();
+  });
+
+  it('should handle user assignment', async () => {
+    renderAcquisitionUnitMembershipsContainer();
+
+    await userEvent.click(screen.getByText(selectUsersLabel));
+
+    expect(defaultProps.mutator.acquisitionsUnitMemberships.POST).toHaveBeenCalledWith(
+      expect.objectContaining({ userId: '1' }),
+    );
+
+    await userEvent.click(screen.getByRole('button', { name: 'ui-acquisition-units.unit.membership.actions.remove' }));
+
+    expect(defaultProps.mutator.acquisitionsUnitMemberships.DELETE).toHaveBeenCalled();
   });
 });
